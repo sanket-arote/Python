702ef9d6d272213f2f605b6a6b1bcc87b54a613b
sanket arote <arotesanket15@gmai.com>
2021-03-12 11:25:49 +0530

    Addition Code

---
+++ Session's Code/Addition.py
@@ -0,0 +1,20 @@
+# price_of_balls is a variable.......................
+price_of_balls = 10
+
+price_of_pencils = 20.09
+
+number_of_balls = 8
+
+number_of_pencils = 10
+
+total = price_of_balls * number_of_balls + price_of_pencils * number_of_pencils
+
+# print will basically print your statement...............................
+print('Welcome to our shop')
+
+print('Please Pay', total)
+
+print('Visit Again')
+
+# it will give you which type of variable we are using...................
+type(number_of_pencils)
