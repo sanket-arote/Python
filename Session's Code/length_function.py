fed33fdd6fef00c60c7624ebff5385b7af1b052a
sanket arote <arotesanket15@gmai.com>
2021-03-14 11:55:26 +0530

    Length Function Code

---
+++ Session's Code/length_function.py
@@ -0,0 +1,23 @@
+var = 'Sanket'
+len(var)
+
+
+
+"""
+>>> char.split(' ')
+['', 'Sanket', 'Dnyaneshwar', 'Arote']
+>>> type(char)
+<class 'str'>
+>>> words = char.split(' ')
+>>> words
+['', 'Sanket', 'Dnyaneshwar', 'Arote']          ###### Split is used Slpit the data
+>>> type(word)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'word' is not defined
+>>> len(words)                                  ####### Lenght is used to calculate lenght & it is a generic function.
+4
+>>> type(words)
+<class 'list'>                 ################## List is one type of advance datatype in Python.  
+
+"""
